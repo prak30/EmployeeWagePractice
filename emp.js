b1bf9fcb580155b6c60121a031bf7e997a1853e2
pranav <prnvkatkar88@gmail.com>
2021-11-25 22:44:38 +0530

    refactored code by using function to calculate part and full time wages

--- emp.js
+++ emp.js
@@ -1,23 +1,22 @@
-//UC2
+//UC3
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-let empHours = 0;
 
-empCheck = Math.floor(Math.random()*10)%3;
-console.log(+empCheck);
-switch(empCheck) {
+function getWorkingHours(empCheck){
+    switch(empCheck){
     case IS_PART_TIME:
-    empHours = PART_TIME_HOURS;
-    break;
+    return PART_TIME_HOURS;
     case IS_FULL_TIME:
-        empHours = FULL_TIME_HOURS;
-        break;
+        return FULL_TIME_HOURS;
         default:
-            empHours = 0;
+            return 0;
+    }
 }
-
+let empHours = 0;
+let empCheck = Math.floor(Math.random()*10)%3;
+empHours = getWorkingHours(empCheck);
 let empWage = empHours*WAGE_PER_HOUR;
 console.log("Emp Wage:" +empWage);
