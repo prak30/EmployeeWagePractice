74258beef6c696cdf8bd533d7b29385c5d5e02db
pranav <prnvkatkar88@gmail.com>
2021-11-25 23:10:48 +0530

    Added:fuctionality to calculate monthly wages

--- emp.js
+++ emp.js
@@ -1,9 +1,10 @@
-//UC3
+//UC4
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUMBER_OF_WORKING_DAYS = 20;
 
 function getWorkingHours(empCheck){
     switch(empCheck){
@@ -16,7 +17,9 @@ function getWorkingHours(empCheck){
     }
 }
 let empHours = 0;
+for(let day = 0; day < NUMBER_OF_WORKING_DAYS; day++){
 let empCheck = Math.floor(Math.random()*10)%3;
-empHours = getWorkingHours(empCheck);
+empHours += getWorkingHours(empCheck);
+}
 let empWage = empHours*WAGE_PER_HOUR;
-console.log("Emp Wage:" +empWage);
+console.log("Total hours:"+empHours+"Emp Wage:" +empWage);
